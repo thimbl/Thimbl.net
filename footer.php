1c117ed8efa1958eb9865135a64f5929af36e1e8
Mike Pearce <mike@mikepearce.net>
2011-01-21 21:53:18 +0000

    Added a header and footer include, thank sandwiches!

---
+++ footer.php
@@ -0,0 +1,23 @@
+<footer>
+	<div class="wrapper">
+		<nav>
+			<h5>
+				SUPPORT THIMBL
+			</h5>
+			<ul>
+				<li id="thimbl">
+					<a href="http://www.drumbeat.org/project/thimbl-decentralized-micro-blogging-platform">Vote for Thimbl</a>
+				</li>
+				<li id="facebook">
+					<a href="http://www.facebook.com/telekommunisten">Like us on Facebook</a>
+				</li>
+				<li id="twitter">
+					<a href="http://www.twitter.com/thimbl">Follow us on Twitter</a>
+				<li id="identica">
+					<a href="http://identi.ca/thimbl">Follow us on Identi.ca</a>
+				</li>
+			</ul>
+			<div style="clear: both;"></div>
+		</nav>
+	</div>
+</footer>
