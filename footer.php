99675b3bbc099d311f6fe8a6ea7caf684fc968a8
Mike Pearce <mike@mikepearce.net>
2011-01-27 23:49:33 +0000

    Added the facebook 'like' button with the dark theme to the footer include

--- footer.php
+++ footer.php
@@ -1,10 +1,13 @@
+
 <footer>
 	<div class="wrapper">
 		<nav>
 			<h5>
 				SUPPORT THIMBL
 			</h5>
+			<iframe src="http://www.facebook.com/plugins/like.php?href=http%3A%2F%2Fthimbl.net&amp;layout=standard&amp;show_faces=false&amp;width=450&amp;action=like&amp;colorscheme=dark&amp;height=35" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:450px; height:35px;" allowTransparency="true"></iframe>
 			<ul>
+			    
 				<li id="thimbl">
 					<a href="http://www.drumbeat.org/project/thimbl-decentralized-micro-blogging-platform">Vote for Thimbl</a>
 				</li>
